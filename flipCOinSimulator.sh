ed045eb4e64f093818c350c9b0920cb4666646e5
krupali <pushkarmorey555@gmail.com>
2020-03-21 11:21:51 +0530

    ADDED: code to find the percentage of singlates

--- flipCOinSimulator.sh
+++ flipCOinSimulator.sh
@@ -4,11 +4,41 @@
 # Author -> Pushkar Morey
 # Date -> 20/03/2020
 
+
+function getHeadOrTail() {
+
 if [[ $((RANDOM%2)) -eq 1 ]]
 then
-      echo "HEADS"
+      coinResult=HEADS
 else
-      echo "TAILS"
+      coinResult=TAILS
 fi
 
+}
 
+
+headCount=0
+tailCount=0
+function  singletCombination() {
+
+for (( index=0; index<$numOfFlip; index++ ))
+do
+		getHeadOrTail
+		if [[ $coinResult == HEADS ]]
+		then
+				singletComb[Heads]=$((${singletComb[Heads]}+1))
+				((headCount++))
+		else
+				singletComb[Tails]=$((${singletComb[Tails]}+1))
+				((tailCount++))
+		fi
+done
+
+headPercent=$((($headCount * 100)/$numOfFlip))
+tailPercent=$((($tailCount * 100)/$numOfFlip))
+echo "HEAD Percentage is:$headPercent%"
+echo "Tails Percentage is:$tailPercent%"
+}
+ 
+read -p "Enter the number of time flipp the coin.." numOfFlip
+singletCombination
